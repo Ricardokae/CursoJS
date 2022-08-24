276fe41a631899ea4b6b9dd5ab6806901dc8bb29
Ricardo bento Caetano <101500710+Ricardokae@users.noreply.github.com>
2022-08-23 21:34:14 -0300

    Create serve.js

---
+++ seção 10 node/aula 92 express views/serve.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const app = express();
+const routes = require('./router');
+const path = require('path');
+
+
+app.use(express.urlencoded( { extended: true } ))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(routes);
+
+
+
+app.listen(100,()=>{
+    console.log('Acessar http://localhost:100');
+})
